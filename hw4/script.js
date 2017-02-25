542e8205d9cf88f6267e0e37bdbad5793a88e2b8
Irene Alvarado <ire.alvarado@gmail.com>
2017-02-25 13:05:51 -0500

    Can add single games to team rows

--- hw4/script.js
+++ hw4/script.js
@@ -11,13 +11,15 @@ var cellWidth = 70,
     cellBuffer = 15,
     barHeight = 20;
 
+var goalCellWidth = 2 * cellWidth - cellBuffer;
+
 /**Set variables for commonly accessed data columns*/
 var goalsMadeHeader = 'Goals Made',
     goalsConcededHeader = 'Goals Conceded';
 
 /** Setup the scales*/
 var goalScale = d3.scaleLinear()
-    .range([cellBuffer, 2 * cellWidth - cellBuffer]);
+    .range([cellBuffer, goalCellWidth]);
 
 /**Used for games/wins/losses*/
 var gameScale = d3.scaleLinear()
@@ -53,12 +55,12 @@ d3.json('data/fifa-matches.json',function(error,data){
         return d.value["TotalGames"];
     })
     gameScale.domain([0,maxGames]);
+    aggregateColorScale.domain([0,maxGames]);
 
     // Set goals scale
     var maxGoals = d3.max(teamData, function(d) {
         return d.value["Delta Goals"] + d.value["Goals Conceded"];
     })
-    console.log(maxGoals);
     goalScale.domain([0,maxGoals]);
 
     createTable();
@@ -86,7 +88,7 @@ d3.csv("data/fifa-tree.csv", function (error, csvData) {
  */
 function createTable() {
     tableElements = teamData;
-    console.log(tableElements);
+    // console.log(tableElements);
 
 // ******* TODO: PART II *******
 
@@ -99,15 +101,24 @@ function createTable() {
  *
  */
 function updateTable() {
+    console.log("\n\n");
+    console.log("UPDATING TABLE AGAIN");
+
     // select the body section of the table
     var table = d3.select('#matchTable').select('tbody');
     // console.log(table.node());
 
-    // Create row for each object in the data
-    var rows = table.selectAll('tr')
-        .data(tableElements)
-        .enter()
-        .append('tr');
+
+    rows = table.selectAll('tr')
+        // Make sure we keep object consistency between our data and HTML Dom objects
+        .data(tableElements, function(d) {
+            return d.key;
+        })
+        .enter().append('tr');
+
+    // Add ability to click on each row
+    rows.on("click", updateList);
+
 
     // Create a cell for each row in each column
     var cells = rows.selectAll('td')
@@ -115,23 +126,47 @@ function updateTable() {
         .data(function(row, i) {
             // console.log(row);
             var result = [];
-            result.push(createCell('text', row.key));
-            result.push(createCell('goals', {
-                "made": row.value["Goals Made"],
-                "lost": row.value["Goals Conceded"],
-                "delta": row.value["Delta Goals"]
-            }));
-            result.push(createCell('text', row.value.Result.label));
-            result.push(createCell('bar', row.value.Wins));
-            result.push(createCell('bar', row.value.Losses));
-            result.push(createCell('bar', row.value.TotalGames));
+
+            if(row.value.type == "aggregate") {
+                result.push(createCell('text', row.key));
+                result.push(createCell('goals', {
+                    "made": row.value["Goals Made"],
+                    "lost": row.value["Goals Conceded"],
+                    "delta": row.value["Delta Goals"]
+                }));
+                result.push(createCell('text', row.value.Result.label));
+                result.push(createCell('bar', row.value.Wins));
+                result.push(createCell('bar', row.value.Losses));
+                result.push(createCell('bar', row.value.TotalGames));
+            }
+
+            if(row.value.type == "game") {
+                // console.log(row);
+                // console.log(row);
+
+                result.push(createCell('text', row.key));
+                result.push(createCell('text', "TEST"));
+                result.push(createCell('text', row.value.Result.label));
+                result.push(createCell('text', "TEST"));
+                result.push(createCell('text', "TEST"));
+                result.push(createCell('text', "TEST"));
+            }
+
             return result;
         })
         .enter()
-        .append('td')
-        .text(function(d) {
-            return d.value;
-        });
+        .append('td');
+        // .attr("width", cellWidth)
+        // .attr("height", cellHeight);
+
+
+    // Get the cells that will have text
+    var cellsWithText = cells.filter(function(d) {
+        return d.vis == 'text';
+    })
+    cellsWithText.text(function(d) {
+        return d.value;
+    });
 
     // Get the cells that will have the bar charts
     var cellsWithBars = cells.filter(function(d) {
@@ -147,7 +182,10 @@ function updateTable() {
         .attr("width", function(d) {
             return gameScale(d.value);
         })
-        .attr("height", barHeight);
+        .attr("height", barHeight)
+        .attr("fill", function(d) {
+            return aggregateColorScale(d.value);
+        });
 
     // Get the cells that will have the bar charts
     var cellsWithGoals = cells.filter(function(d) {
@@ -155,9 +193,12 @@ function updateTable() {
     })
     // console.log(cellsWithBars);
 
+    // Update the width of the cell
+    cellsWithGoals.attr("width", goalCellWidth);
+
     cellsWithGoals.append("svg")
         // Attributes of SVG
-        .attr("width", cellWidth)
+        .attr("width", goalCellWidth)
         .attr("height", cellHeight)
         // Inside each svg add a rect for the bar
         .append("rect")
@@ -169,9 +210,13 @@ function updateTable() {
             // The width will be the delta of made goals - lost goals
             return goalScale(Math.abs(d.value.delta));
         })
-        .attr("height", barHeight);
-
-// ******* TODO: PART III *******
+        .attr("height", barHeight)
+        .attr("fill", function(d) {
+            return goalColorScale(d.value.delta);
+        })
+        //Rounding corners of rect
+        .attr("rx", 10)
+        .attr("ry", 10);
 
 };
 
@@ -189,7 +234,9 @@ function createCell(vis, value) {
  * Collapses all expanded countries, leaving only rows for aggregate values per country.
  *
  */
-function collapseList() {
+function collapseList(i) {
+    console.log(tableElements);
+    console.log(i)
 
     // ******* TODO: PART IV *******
 
@@ -200,7 +247,36 @@ function collapseList() {
  * Updates the global tableElements variable, with a row for each row to be rendered in the table.
  *
  */
-function updateList(i) {
+function updateList(currentRow, i) {
+    // console.log(tableElements.length);
+    nextRow = tableElements[i+1];
+    // console.log(nextRow);
+
+    // If the currently clicked row is a team, not a game
+    if (currentRow.value.type == "aggregate") {
+        // If the next row is also a team, then expand on games
+        if(nextRow != undefined && nextRow.value.type == "aggregate") {
+            var games = currentRow.value.games;
+            // console.log("GAMES TO ADD");
+            // console.log(games);
+
+            for(var j = 0 ; j < games.length ; j++) {
+                // Updating keys in the games data so that it doesn't match with another existing team
+                games[j].key = "x" + games[j].key;
+                tableElements.splice((i+1+j), 0, games[j]);
+            }
+            // console.log(tableElements);
+        }
+        // If the next row is a game, we want to collapse the row
+        if(nextRow != undefined && nextRow.value.type == "game") {
+            collapseList(i);
+        }
+    }
+
+    // console.log("\n\nNEW TABLE")
+    // console.log(tableElements, tableElements.length);
+
+    updateTable();
 
     // ******* TODO: PART IV *******
 
