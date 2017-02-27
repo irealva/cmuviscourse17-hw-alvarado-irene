d5aeeae1e436f0989c84f94dcacd2992953d70b8
Irene Alvarado <ire.alvarado@gmail.com>
2017-02-26 20:09:09 -0500

    Basic working tree

--- hw4/script.js
+++ hw4/script.js
@@ -414,34 +414,7 @@ function createTree(treeData) {
             return pathing;
         });
 
-    // var link = svg.selectAll(".link")
-    //   .data(root.descendants().slice(1))
-    // .enter().append("path")
-    //   .attr("class", "link")
-    //   .attr("d", function(d) {
-    //     return "M" + project(d.x, d.y)
-    //         + "C" + project(d.x, (d.y + d.parent.y) / 2)
-    //         + " " + project(d.parent.x, (d.y + d.parent.y) / 2)
-    //         + " " + project(d.parent.x, d.parent.y);
-    //   });
-
-    // var node = svg.selectAll(".node")
-    //   .data(root.descendants())
-    //   .enter().append("g")
-    //   .attr("class", function(d) { return "node" + (d.children ? " node--internal" : " node--leaf"); })
-    //   .attr("transform", function(d) { return "translate(" + project(d.x, d.y) + ")"; });
-    //
-    //   node.append("circle")
-    //   .attr("r", 2.5);
-
 };
-
-
-function project(x, y) {
-    var angle = (x - 90) / 180 * Math.PI,
-        radius = y;
-    return [radius * Math.cos(angle), radius * Math.sin(angle)];
-}
 
 /**
  * Updates the highlighting in the tree based on the selected team.
